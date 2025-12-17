659c8b36d1b268d83c03b46d10a117703e559c0d
QIWEI-LIANG <2908375382@qq.com>
2025-12-16 23:26:10 -0800

    delete 没有用的版本

--- Grand/vue-project/src/views/public/PublicHome.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page">
-    <h1>Welcome to the Graduate School Portal</h1>
-    <div class="card">
-      <h3>Announcements</h3>
-      <p>Fall 2025 thesis submission deadline is December 1st.</p>
-    </div>
-  </div>
-</template>
